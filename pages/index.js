3e8d0c89f56bcae79ccfd1199efcf5ab07343dee
Parth <parolekarparth@gmail.com>
2023-01-31 15:46:01 +0530

    tailwind setup

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+  );
+}
